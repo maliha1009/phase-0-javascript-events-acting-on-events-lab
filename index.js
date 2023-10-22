32c23587831c1ab305eba905146e1e20ab768ef7
maliha1009 <90807398+maliha1009@users.noreply.github.com>
2023-10-22 13:02:49 -0400

    solution

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+let dodger = document.getElementById('dodger');
+
+const speed = 1;  // Change this value to manage the speed of the dodger
+
+function moveDodgerLeft() {
+    let leftPosition = parseInt(dodger.style.left, 10);
+
+    if (leftPosition > 0){
+        dodger.style.left = `${leftPosition - speed}px`;
+    }
+}
+
+function moveDodgerRight() {
+    let leftPosition = parseInt(dodger.style.left, 10);
+
+    if (leftPosition < 360){
+        dodger.style.left = `${leftPosition + speed}px`;
+    }
+}
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (event.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+});
